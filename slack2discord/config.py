32111248a3213c5a0dae07dd45160f3678e6184b
Rich Fromm <github@richard.fromm.name>
2022-09-16 22:17:40 -0700

    update README
    nearly a complete rewrite
    to account for the hard fork and refactoring

--- slack2discord/config.py
+++ slack2discord/config.py
@@ -14,8 +14,6 @@ DESCRIPTION = dedent(
     slack2discord parses data exported from Slack, and imports it to Discord.
     """)
 
-# XXX TO DO:
-# - consider adding the ability for the script to create channels
 USAGE = dedent(
     f"""
     {argv[0]} [--token TOKEN] [-v | --verbose] [-n | --dry-run] <src-and-test-related-options>
