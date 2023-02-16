8b0283e5e81f98e07994def8eff3dd9554db68a8
Rich Fromm <github@richard.fromm.name>
2023-02-15 21:17:04 -0800

    abstract flake8 and mypy checks to a separate script
    the script is check.sh
    
    this is b/c `continue-on-error` doesn't behave as I had guessed
    it allows for failure
    
    what I want is for any failure to be marked as such
    but for a failure to **not** abort the checks
    I want all checks to run regardless
    I could do this by splitting up into multiple checks
    but that seems overkill
    
    this has the added bonus of being a script that users can run interactively

---
+++ check.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# this assumes that all project dependencies, as well as all dev dependencies
+# (e.g. flake8 and mypy) have already been installed, e.g. via:
+#    pip install -r requrements.txt
+#    pip install -r requrements-dev.txt
+
+# flake8 and mypy config options are specified in setup.cfg
+
+# set to a non-zero value if any check returns a non-zero value
+check_result=0
+
+echo "Run flake8 style checks"
+echo "flake8 slack2discord.py slack2discord"
+flake8 slack2discord.py slack2discord
+flake8_result=$?
+if [[ $flake8_result -eq 0 ]]; then
+   echo "flake8: PASS"
+else
+   echo "flake8: FAIL"
+   check_result=1
+fi
+
+echo
+echo "Run mypy static typing checks"
+echo "mypy slack2discord.py"
+mypy slack2discord.py
+mypy_result=$?
+if [[ $mypy_result -eq 0 ]]; then
+   echo "mypy: PASS"
+else
+   echo "mypy: FAIL"
+   check_result=1
+fi
+
+echo
+if [[ $check_result -eq 0 ]]; then
+   echo "Union of all checks: PASS"
+else
+   echo "Union of all checks: FAIL"
+fi
+
+exit $check_result
