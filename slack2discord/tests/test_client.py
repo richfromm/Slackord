3ece37822d94d59d0571830c47a75bd2de2d17b1
Rich Fromm <github@richard.fromm.name>
2023-02-20 18:27:53 -0800

    add more parametrized test cases

--- slack2discord/tests/test_client.py
+++ slack2discord/tests/test_client.py
@@ -12,10 +12,12 @@ class TestDiscordClient():
         "foo_-bar",
         "foo__bar",
         "foo-_-bar",
+        "foo_-_bar",
         "foo-bar-baz",
         "foo_bar_baz",
         "foo-bar_baz",
         "foo_bar-baz",
+        "a",
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_",
         "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789",  # noqa: E501
         "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",  # noqa: E501
@@ -28,8 +30,17 @@ class TestDiscordClient():
 
     @pytest.mark.parametrize("channel_name", [
         "foo--bar",
+        "foo---bar",
+        "foo----bar",
         "",
+        " ",
+        "foo bar",
+        "foo	bar",
         "foo#bar",
+        "foo`-=bar",
+        "foo~!@#$%^&*()_+bar",
+        r"foo[]\;',./bar",  # don't want the backslash to be treated as an escape
+        'foo{}|:"<>?bar',
         "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",  # noqa: E501
     ])
     def test_invalid_channel_name(self, channel_name):
